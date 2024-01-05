cbc9f1907f173abe9d056a4d57d0ec80b5e1e593
Pyxis1321 <kopas1307@gmail.com>
2024-01-05 19:59:40 +0100

    added splash screen and changed app icon

---
+++ app/src/main/java/com/example/dogapp/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.dogapp // This should match your package structure
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000) // 3000 milliseconds delay for the splash screen
+    }
+}
